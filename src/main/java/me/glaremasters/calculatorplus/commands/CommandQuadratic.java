c69151f561bb0712ec0e35dbf93935586eb494df
darbyjack <admin@glaremasters.me>
2018-03-20 20:13:27 -0500

    Started working on quadratic tooltips

--- src/main/java/me/glaremasters/calculatorplus/commands/CommandQuadratic.java
+++ src/main/java/me/glaremasters/calculatorplus/commands/CommandQuadratic.java
@@ -1,9 +1,12 @@
 package me.glaremasters.calculatorplus.commands;
 
 import static me.glaremasters.calculatorplus.util.ColorUtil.color;
+import static me.glaremasters.calculatorplus.util.ColorUtil.color2;
 import java.text.DecimalFormat;
 import me.glaremasters.calculatorplus.CalculatorPlus;
 import me.glaremasters.calculatorplus.commands.base.CommandBase;
+import me.rayzr522.jsonmessage.JSONMessage;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
@@ -24,6 +27,8 @@ public class CommandQuadratic extends CommandBase {
             a = Integer.valueOf(args[0]);
             b = Integer.valueOf(args[1]);
             c = Integer.valueOf(args[2]);
+            String signs = config.getString("colors.signs");
+            String inputs = config.getString("colors.inputs");
 
             d = b * b - 4 * a * c;
             DecimalFormat df = new DecimalFormat("#.###");
@@ -35,9 +40,13 @@ public class CommandQuadratic extends CommandBase {
 
                 String r1Format = df.format(r1);
                 String r2Format = df.format(r2);
-                player.sendMessage(color(config.getString("messages.roots")
-                        .replace("{root1}", r1Format)
-                        .replace("{root2}", r2Format)));
+                JSONMessage.create(color(
+                        config.getString("messages.roots").replace("{root1}", r1Format)
+                                .replace("{root2}", r2Format))).tooltip(
+                        color2(config.getString("messages.solution") + ChatColor.RESET
+                                + "\nRoot One: " + signs + "-" + inputs + b + signs + " + √(" + inputs
+                                + d + signs + ") / (2 * " + inputs + a + signs + ")" + ChatColor.RESET + "\nRoot Two: " + signs + "-" + inputs + b + signs + " - √(" + inputs
+                                + d + signs + ") / (2 * " + inputs + a + signs + ")")).send(player);
             } else if (d == 0) {
                 player.sendMessage(color(config.getString("messages.real-equal")));
                 r1 = (-b + Math.sqrt(d)) / (2 * a);
